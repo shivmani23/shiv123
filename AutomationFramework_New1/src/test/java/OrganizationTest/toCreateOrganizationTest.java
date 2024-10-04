c9c28f645dddd30d55a63be6c078ec9f20eaad18
USER <USER@DESKTOP-EMMLUNC>
2024-10-04 13:39:55 +0530

    pushing framework

---
+++ AutomationFramework_New1/src/test/java/OrganizationTest/toCreateOrganizationTest.java
@@ -0,0 +1,8 @@
+package OrganizationTest;
+
+public class toCreateOrganizationTest {
+	public static void main(String[] args) {
+		System.out.println("organization test has been completed");
+	}
+
+}
---
+++ AutomationFramework_New1/src/test/java/contactTest/toCeateContactTest.java
@@ -0,0 +1,8 @@
+package contactTest;
+
+public class toCeateContactTest {
+	public static void main(String[] args) {
+		System.out.println("this test cases is validated");
+	}
+
+}
